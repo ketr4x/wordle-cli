2c39e648198669f9bc17017519f20c2836bf6379
ketr4x <ketraxpl@gmail.com>
2025-10-14 21:30:16 +0200

    Use SQLAlchemy's text() for executing auth column migration query

--- server/migrate_auth_column.py
+++ server/migrate_auth_column.py
@@ -1,8 +1,9 @@
 from server.app import app, db
+from sqlalchemy import text
 
 def migrate_auth_column():
     with app.app_context():
-        db.session.execute('ALTER TABLE "user" ALTER COLUMN auth TYPE VARCHAR(256);')
+        db.session.execute(text('ALTER TABLE "user" ALTER COLUMN auth TYPE VARCHAR(256);'))
         db.session.commit()
         print("User.auth column updated to VARCHAR(256)")
 

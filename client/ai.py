521e38b54df92b2e4e427445e9bff5a3895be690
ketr4x <ketraxpl@gmail.com>
2025-10-16 20:40:07 +0200

    Add AI word generation feature.

---
+++ client/ai.py
@@ -0,0 +1,39 @@
+from randomword import *
+import http.client
+
+# Courtesy of HackClub - do not abuse https://ai.hackclub.com/
+def get_ai_word(language):
+    conn = http.client.HTTPSConnection("ai.hackclub.com")
+    payload = f'{{"messages":[{{"content":"Generate me a 5-letter wordle answer for this language: {language}","role":"user"}}]}}'
+    headers = { 'Content-Type': "application/json" }
+    conn.request("POST", "/chat/completions", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    return data.decode("utf-8")
+
+def settings():
+    utils.clear_screen()
+    length = 5
+    language = ""
+    languages = list(map(str, utils.languages()))
+    while language not in languages:
+        language = (input(f"What language do you want your word? ({','.join(languages)}) ") or "en").strip().lower()
+    return length, language
+
+def game_ai():
+    length, language = settings()
+    word = get_ai_word(language)
+    filtered = sorted(utils.wordlist(language))
+    game_status, guesses, formatted_guesses, letters = game(word, filtered, 6, language)
+
+    utils.clear_screen()
+    if game_status == 2:
+        print(f"You won in {len(guesses)} guesses!")
+    else:
+        print(f"You lost!")
+    print(f"Your guesses were:")
+    for i in formatted_guesses:
+        print(i)
+    print(f"The word was {word}")
+    print(f"You had {len(letters)} letters remaining")
+    input("\nPress `Enter` to continue...")
--- readme.md
+++ readme.md
@@ -2,8 +2,9 @@
 ## Features
 - Custom solo game
 - Daily game
-- Online game
+- Online (ranked) game
 - Multi-language support
+- File synchronization
 - Statistics
 - Leaderboards
 - A flutter version for all platforms
@@ -23,6 +24,7 @@
 - Click releases
 - Download the latest file for your OS
 - Install it
+- Set up your account and server (below)
 #### Website
 - Go to http://wordle.ketrax.ovh/ or https://afternoon-waters-00138-898a825f9a47.herokuapp.com/
 ### Server
@@ -35,6 +37,8 @@
 - `heroku local --port 5006 -f Procfile.windows` or `flask --app server/app.py run`
 ## Planned features
 - ?
+## License
+Copyright ketr4x, 2025. Licensed under BSD-3-Clause License.
 
 This project was made for the [Moonshot](https://moonshot.hack.club/1016) hackathon organized by HackClub.
 ![Hackatime Badge](https://hackatime-badge.hackclub.com/U08RQEP53HA/wordle-cli)
